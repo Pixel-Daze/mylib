277deeddb61f9c6350244b44b661951d9a321b7a
Pixel-Daze <1226093323@qq.com>
2021-01-03 23:00:23 +0800

    sth

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Area = styled.div`
+  color: blue;
+`;
+
+// Delete me
+export const Thing = () => {
+  return <div>the snozzberries taste like snozzberries<Area>123</Area></div>;
+};
